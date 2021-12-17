231cc3371d34c37bb259929e253cbae1b8bc42f9
Ashish <ashushrm864@gmail.com>
2021-12-17 23:49:15 +0530

    Created User Schema

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
 
 const app = express();
+
+//const User=require('./models/userSchema');
+
+dotenv.config({path:'./config.env'});
+require('./db/conn');
+
+const PORT=process.env.PORT;
+
 
 app.get('/' , (req,res)=>{
     res.send("Hello World from server app.js");
 });
 
+app.get('/login' , (req,res)=>{
+    res.send("Hello World On Login Page");
+});
 
-app.listen(3000, ()=>{
-    console.log(`Server started on port 3000`);
+app.get('/register' , (req,res)=>{
+    res.send("Hello World On Signup Page");
+});
+
+app.get('/about' , (req,res)=>{
+    res.send("Hello World On About Page");
+});
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server started on port ${PORT}`);
 })
